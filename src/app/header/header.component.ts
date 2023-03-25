877fe2c5846d91a071dce90dbb358075663ac6c4
James Welander <jameswelander@me.com>
2023-03-25 16:34:10 -0500

    basic framework started

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+}
